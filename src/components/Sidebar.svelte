0d52eff384c0ac57bad7b02bb010833aadc2c946
Mika Syynimaa <mika.syynimaa@gmail.com>
2021-02-13 19:18:05 +0200

    Updated artist icon, added new data on stamp page.

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -50,7 +50,7 @@
       </SidebarLink>
       <SidebarLink title="Taiteilijat" href="taiteilijat" segment={segment}>
         <svg slot="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
         </svg>
       </SidebarLink>
       <SidebarLink title="Painopaikat" href="painopaikat" segment={segment}>
