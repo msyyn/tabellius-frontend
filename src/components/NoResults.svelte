1d93a260f05e0064cdd90d944afceeaf3c50ff67
Mika Syynimaa <mika.syynimaa@gmail.com>
2021-02-13 15:39:11 +0200

    Fixed issue on no results page.

--- src/components/NoResults.svelte
+++ src/components/NoResults.svelte
@@ -1,7 +1,7 @@
 <script>
   export let page;
+  import paginate from '../utils/paginate.js';
   import { goto } from '@sapper/app'; 
-  import { paginate } from '../utils/paginate.js';
   function resetPage() {
     goto(page.currentPath, true, true);
     page.visibleData = paginate(page.data, page.paginateBy, page.currentPage);
@@ -9,8 +9,8 @@
   };
 </script>
 <div class="lg:w-1/3 my-16 mx-auto flex flex-col justify-center">
-  <h2 class="text-xl tracking-wide font-bold mb-2 text-red-400">Hakutuloksia löytäneet, emme ☹</h2>
-  <p class="text-lg mb-6 text-gray-500">Harmin lista :--(</p>
+  <h2 class="text-xl tracking-wide font-bold mb-2 text-red-400">Ei hakutuloksia :(</h2>
+  <p class="text-lg mb-6 text-gray-500">Ole hyvä ja poista rajaukset.</p>
   <button title="Poista rajaukset" on:click={resetPage} class="transition duration-300 ease-linear p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-100 hover:bg-indigo-200 text-indigo-500 bg-indigo-50">
     Poista rajaukset
   </button>
