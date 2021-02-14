b239a713f9fa1719211f7a24ee41744095c7a60d
Mika Syynimaa <mika.syynimaa@gmail.com>
2021-02-14 02:16:56 +0200

    Small improvement on text readability

--- src/routes/postimerkit/[handle].svelte
+++ src/routes/postimerkit/[handle].svelte
@@ -99,51 +99,51 @@
 
       <!-- Perustiedot, ikonit -->
       <div class="mt-6 flex flex-wrap flex-col md:flex-row lg:flex-row">
-        <div class="md:w-1/2 lg:w-1/2 mb-4 transition duration-300 ease-linear bg-transparent hover:text-indigo-500 text-indigo-300 focus:outline-none focus:ring-2 focus:ring-indigo-100 flex items-center relative">
+        <div class="md:w-1/2 lg:w-1/2 mb-4 transition duration-300 ease-linear bg-transparent hover:text-indigo-400 text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-100 flex items-center relative">
           <div class="w-10 h-10 p-2 rounded-lg text-indigo-300 bg-indigo-50">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
             </svg>
           </div>
           <a title="Taiteilijan {stamp.taiteilija} koostesivu" href="/taiteilijat/{handleize(stamp.taiteilija)}" class="ml-4">
-            <span class="text-gray-300 text-xs uppercase tracking-wide block">Taiteilija</span>
+            <span class="text-gray-500 text-xs uppercase tracking-wide block">Taiteilija</span>
             <span class="font-medium tracking-wide text-sm block">{stamp.taiteilija}</span>
           </a>
         </div>
 
-        <div class="md:w-1/2 lg:w-1/2 mb-4 transition duration-300 ease-linear bg-transparent hover:text-indigo-500 text-indigo-300 focus:outline-none focus:ring-2 focus:ring-indigo-100 flex items-center relative">
+        <div class="md:w-1/2 lg:w-1/2 mb-4 transition duration-300 ease-linear bg-transparent hover:text-indigo-400 text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-100 flex items-center relative">
           <div class="w-10 h-10 p-2 rounded-lg text-indigo-300 bg-indigo-50">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
             </svg>
           </div>
           <a title="Painopaikan {stamp.painopaikka} koostesivu" href="/painopaikat/{handleize(stamp.painopaikka)}" class="ml-4">
-            <span class="text-gray-300 text-xs uppercase tracking-wide block">Painopaikka</span>
+            <span class="text-gray-500 text-xs uppercase tracking-wide block">Painopaikka</span>
             <span class="font-medium tracking-wide text-sm block">{stamp.painopaikka}</span>
           </a>
         </div>
 
-        <div class="md:w-1/2 lg:w-1/2 mb-4 transition duration-300 ease-linear bg-transparent text-indigo-300 focus:outline-none focus:ring-2 focus:ring-indigo-100 flex items-center relative">
+        <div class="md:w-1/2 lg:w-1/2 mb-4 transition duration-300 ease-linear bg-transparent text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-100 flex items-center relative">
           <div class="w-10 h-10 p-2 rounded-lg text-indigo-300 bg-indigo-50">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
             </svg>
           </div>
           <span class="ml-4">
-            <span class="text-gray-300 text-xs uppercase tracking-wide block">Painosmäärä</span>
+            <span class="text-gray-500 text-xs uppercase tracking-wide block">Painosmäärä</span>
             <span class="font-medium tracking-wide text-sm block">{stamp.currentVariant.painosmaara ? `${stamp.currentVariant.painosmaara.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} kpl` : 'Ei tiedossa'}</span>
           </span>
         </div>
 
         {#if stamp.currentVariant.nimellisarvo}
-          <div class="md:w-1/2 lg:w-1/2 mb-4 transition duration-300 ease-linear bg-transparent text-indigo-300 focus:outline-none focus:ring-2 focus:ring-indigo-100 flex items-center relative">
+          <div class="md:w-1/2 lg:w-1/2 mb-4 transition duration-300 ease-linear bg-transparent text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-100 flex items-center relative">
             <div class="w-10 h-10 p-2 rounded-lg text-indigo-300 bg-indigo-50">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
             </div>
             <span class="ml-4">
-              <span class="text-gray-300 text-xs uppercase tracking-wide block">Nimellisarvo</span>
+              <span class="text-gray-500 text-xs uppercase tracking-wide block">Nimellisarvo</span>
               <span class="font-medium tracking-wide text-sm block">{stamp.currentVariant.nimellisarvo}{stamp.valuutta ? ` ${stamp.valuutta}` : ' (valuutta ei tiedossa)'}</span>
             </span>
           </div>
