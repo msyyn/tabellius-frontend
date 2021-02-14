277685468dd28e206d47dc7316e0fe12db5d0af3
Mika Syynimaa <mika.syynimaa@gmail.com>
2021-02-14 18:11:38 +0200

    Removed commented out code.

--- src/routes/taiteilijat/[handle].svelte
+++ src/routes/taiteilijat/[handle].svelte
@@ -84,22 +84,6 @@
 
 <Toolbar bind:page/>
 <Pagination bind:page/>
-<!--
-{#if !page.currentHandle}
-  <h1 class="transition duration-300 ease-linear text-lg font-bold text-gray-700 hover:text-indigo-500"><a rel="prefetch" href="/taiteilijat/{handleize(artist.nimi)}" title="Taiteilijan {artist.nimi} koostesivu">{artist.nimi}</a></h1>
-  <div class="flex flex-row items-center justify-between">
-    <span class="text-gray-500 text-sm">Näytetään { artist.stamps.length > limit ? limit : artist.stamps.length } / {artist.stamps.length} postimerkkiä</span>
-    {#if artist.stamps.length > limit}
-      <a rel="prefetch" href="/taiteilijat/{handleize(artist.nimi)}" title="Taiteilijan {artist.nimi} koostesivu" class="transition duration-300 ease-linear py-1 px-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-100 flex flex-row items-center hover:bg-indigo-200 text-indigo-500 bg-indigo-50">
-        <span class="text-sm">Näytä kaikki</span>
-        <svg class="ml-1 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-        </svg>
-      </a>
-    {/if}
-  </div>
-{:else}
--->
 <div class="text-sm tracking-wide mt-4">
   <a class="transition duration-300 ease-linear font-bold text-indigo-300 hover:text-indigo-500" href="{page.currentPath.replace(page.currentHandle,'')}" title="{breadcrumb}">{breadcrumb}</a>
   <span class="text-gray-400 text-xs">/</span>
